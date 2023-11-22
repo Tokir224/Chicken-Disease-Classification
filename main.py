4433413c4cec22bb23af266016445288e10a1430
Tokir-Vora <tokirvora.224@gmail.com>
2023-11-22 17:48:27 +0530

    training pipeline added

--- main.py
+++ main.py
@@ -3,28 +3,40 @@ from chicken_disease.pipeline.stage_01_data_ingestion import DataIngestionPipeli
 from chicken_disease.pipeline.stage_02_prepare_base_model import (
     PrepareBaseModelTrainingPipeline,
 )
-
+from chicken_disease.pipeline.stage_03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data ingestion stage"
 
 try:
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = DataIngestionPipeline()
-    obj.main()
+    data_ingestion = DataIngestionPipeline()
+    data_ingestion.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx===================x")
 except Exception as e:
     logger.exception(e)
     raise e
 
 
-STAGE_NAME = "Data ingestion stage"
+STAGE_NAME = "Prepare base model"
 
 try:
     logger.info("*****************************************")
     logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-    obj = PrepareBaseModelTrainingPipeline()
-    obj.main()
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx===================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Training"
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
