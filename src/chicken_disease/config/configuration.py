4f113862b3aa614bcd24a6144fe8adae60e394b4
Tokir-Vora <tokirvora.224@gmail.com>
2023-11-23 10:59:42 +0530

    evaluation pipeline added

--- src/chicken_disease/config/configuration.py
+++ src/chicken_disease/config/configuration.py
@@ -6,6 +6,7 @@ from chicken_disease.entity.config_entity import (
     PrepareBaseModelConfig,
     PrepareCallbacksConfig,
     TrainingConfig,
+    EvaluationConfig,
 )
 from chicken_disease.utils.common import read_yaml, create_directories
 
@@ -92,3 +93,14 @@ class ConfigurationManager:
         )
 
         return prepare_callback_config
+
+    def get_validation_config(self) -> EvaluationConfig:
+        eval_config = EvaluationConfig(
+            path_of_model="artifacts/training/model.h5",
+            training_data="artifacts/data_ingestion/Chicken-fecal-images",
+            all_params=self.params,
+            params_image_size=self.params.IMAGE_SIZE,
+            params_batch_size=self.params.BATCH_SIZE,
+        )
+
+        return eval_config
